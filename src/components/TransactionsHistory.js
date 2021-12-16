4b4cc1d32553f44dc2d9abce6ac51c89105fd48e
Dawid Cieszyński <dcieszynski99@gmail.com>
2021-12-16 02:25:57 +0100

    update display table

--- src/components/TransactionsHistory.js
+++ src/components/TransactionsHistory.js
@@ -10,27 +10,40 @@ const TransactionsHistory = (props) => {
       </h2>
       <div className='table'>
         <div className='table-headers'>
-          <h3 className='header header--table'>From address</h3>
-          <h3 className='header header--table'>To address</h3>
-          <h3 className='header header--table'>Transfered Value</h3>
+          <div className='table__col'>
+            <h3 className='header header--table'>From address</h3>
+            {props.transactionsHistory.map((tx, i) => {
+              return (
+                <div className='table-col' key={i}>
+                  {tx.from}
+                </div>
+              );
+            })}
+          </div>
+          <div className='table__col'>
+            <h3 className='header header--table'>To address</h3>
+            {props.transactionsHistory.map((tx, i) => {
+              return (
+                <div className='table-col' key={i}>
+                  {tx.to}
+                </div>
+              );
+            })}
+          </div>
+          <div className='table__col'>
+            <h3 className='header header--table'>Value</h3>
+            {props.transactionsHistory.map((tx, i) => {
+              return (
+                <div className='table-col' key={i}>
+                  {parseFloat(
+                    ethers.utils.formatEther(BigNumber.from(tx.value._hex))
+                  ).toFixed(4)}{' '}
+                  ETH
+                </div>
+              );
+            })}
+          </div>
         </div>
-        {props.transactionsHistory.map((transaction, i) => {
-          //Swap to functions
-          return (
-            <div className='table-row' key={i}>
-              <div className='table-col'>{transaction.from}</div>
-              <div className='table-col'>{transaction.to}</div>
-              <div className='table-col'>
-                {parseFloat(
-                  ethers.utils.formatEther(
-                    BigNumber.from(transaction.value._hex)
-                  )
-                ).toFixed(4)}{' '}
-                ETH
-              </div>
-            </div>
-          );
-        })}
       </div>
     </div>
   );
